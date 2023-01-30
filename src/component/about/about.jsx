01fef0d4e648b9a6cedf997f7d80e58fdd53822c
Distructor2404 <101471513+Distructor2404@users.noreply.github.com>
2023-01-30 15:04:29 +0530

    Update about.jsx

--- src/component/about/about.jsx
+++ src/component/about/about.jsx
@@ -42,10 +42,10 @@ const about = () => {
 
             <p>
             Having a Simple and Professional Attitide. Always ready to accept any Challenge. I accept the failure to rise up with more Acceleration.
-            Always used to be a Tech Lover from Birth. Having a great interest in Hardware and Networking, I Love to
+            I am a Tech Lover by Birth. I am having a great interest in Hardware and Networking, I Love to
 deal with all kinds of Computer Related work and
-troubleshooting in Windows. Also Have a Brief
-Knowledge with regards to Front-end Web
+troubleshooting in Windows. I also have a brief
+Knowledge regarding Front-end Web
 Development (React, Html, Css). <br/>
 Learning new things is my Passion. I used to do any work with my 100% effort. I am fond of programming and all sorts of graphics works. Always in a step to move forward for my goal.
             </p>
