62de2add097e68f9e79b4f185060c510b2e98fc2
Distructor2404 <101471513+Distructor2404@users.noreply.github.com>
2023-01-30 15:14:28 +0530

    Update portfolio.jsx

--- src/component/portfolio/portfolio.jsx
+++ src/component/portfolio/portfolio.jsx
@@ -23,10 +23,10 @@ const portfolio = () => {
           <div className="portfolio__item-image">
               <img src={IMG1} alt='' />
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>Travel & Tour React Website</h3>
           <div className="portfolio__item-cta">
-          <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href="https://github.com/Distructor2404/Travel_Website" className='btn' target='_blank'>Github</a>
+          <a href="https://distructor2404.github.io/Travel_Website/" className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article>
 
